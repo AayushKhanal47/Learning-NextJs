8ef562eedb057b8249b461a3dec9d527a436f50f
AayushKhanal47 <aayushkhanal810@gmail.com>
2025-08-22 11:28:18 +0530

    g

--- backend/app/page.tsx
+++ backend/app/page.tsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 async function getUserDetails (){
-  const response = await axios.get("https://backend.aayushkhanal810.workers.dev")
+  const response = await axios.get("https://backend.aayushkhanal810.workers.dev/api/v1/userDetails")
   return response.data
 }
 
