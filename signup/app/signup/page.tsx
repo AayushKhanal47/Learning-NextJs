68aa0e3aeb0b8c409099d627dd7daeead03ff3fb
AayushKhanal47 <aayushkhanal810@gmail.com>
2025-08-23 22:04:01 +0530

    signup

---
+++ signup/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function Signup(){
+    return(
+        <div classame ="flex justify center items-center h-screen bg-color-grey-100 ">
+            <input type= "text" placeholder = "Username" />
+            <input type = "text" placeholder = "Password" />
+            <button>Signup</button>
+        </div>
+    )
+}
